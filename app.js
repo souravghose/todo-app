c519488622f114e223549e98fe54726228d24cc5
Sourav Ghose <66877167+souravghose@users.noreply.github.com>
2021-05-31 21:16:57 +0600

    Update app.js

--- app.js
+++ app.js
@@ -145,9 +145,24 @@ function removeFromLS(taskItem){
 
 let dateMonthYear = new Date();
 let date = dateMonthYear.getDate();
-let month = dateMonthYear.getMonth();
 let year = dateMonthYear.getFullYear();
 var n = dateMonthYear.getUTCDay();
+//get date
+ var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+
+   let month = month[dateMonthYear.getMonth()];
 
 
 if(n==0){
